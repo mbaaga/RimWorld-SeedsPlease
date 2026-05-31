eeb126ed4caac4750826701f77eec230f7d3b6b7
Simon Brennecke <simon@brennecke-it.net>
2026-05-31 16:25:44 +0200

    v11

--- Source/Patches/Patch_SeedExtractionBillGizmos.cs
+++ Source/Patches/Patch_SeedExtractionBillGizmos.cs
@@ -3,7 +3,6 @@ namespace SeedsPleaseLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using HarmonyLib;
 using RimWorld;
 using UnityEngine;
@@ -15,8 +14,6 @@ public static class Patch_Building_GetGizmos
 {
 	private static readonly Texture2D add_bill_icon = ContentFinder<Texture2D>.Get("UI/Commands/SPL_AddSeedExtractionBill", true);
 	private static readonly Texture2D add_missing_bills_icon = ContentFinder<Texture2D>.Get("UI/Commands/SPL_AddMissingSeedExtractionBills", true);
-	private static readonly PropertyInfo bill_renamable_label_property = AccessTools.Property(typeof(Bill_Production), "RenamableLabel");
-	private static readonly MethodInfo bill_renamable_label_setter = AccessTools.PropertySetter(typeof(Bill_Production), "RenamableLabel");
 
 	public static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> __result, Building __instance)
 	{
@@ -156,101 +153,30 @@ public static class Patch_Building_GetGizmos
 	private static string GetBillLabel(ThingDef produce_def)
 	{
 		ThingDef seed_def = GetSeedDef(produce_def);
-		return "SPL.Bill.ExtractSeedsLabel".Translate(seed_def?.label ?? produce_def.label);
+		string seed_label = (seed_def?.label ?? produce_def.label ?? "?").ToLowerInvariant();
+		string label = "SPL.Bill.ExtractSeedsLabel".Translate(seed_label);
+
+		return CapitalizeFirstLetter(label);
+	}
+
+	private static string CapitalizeFirstLetter(string value)
+	{
+		if (string.IsNullOrEmpty(value))
+		{
+			return value;
+		}
+
+		if (value.Length == 1)
+		{
+			return value.ToUpperInvariant();
+		}
+
+		return char.ToUpperInvariant(value[0]) + value.Substring(1);
 	}
 
 	private static void SetBillName(Bill_Production bill, string label)
 	{
-		SetNativeBillName(bill, label);
-		SetBetterWorkbenchManagementBillName(bill, label);
-	}
-
-	private static bool SetNativeBillName(Bill_Production bill, string label)
-	{
-		if (bill == null)
-		{
-			return false;
-		}
-
-		if (bill_renamable_label_setter != null)
-		{
-			bill_renamable_label_setter.Invoke(bill, new object[] { label });
-			return true;
-		}
-
-		if (bill_renamable_label_property?.CanWrite == true)
-		{
-			bill_renamable_label_property.SetValue(bill, label, null);
-			return true;
-		}
-
-		return SetFirstStringField(bill, label,
-			"playerCustomName",
-			"customName",
-			"untranslatedCustomLabel",
-			"customLabel");
-	}
-
-	private static bool SetFirstStringField(Bill_Production bill, string label, params string[] field_names)
-	{
-		foreach (string field_name in field_names)
-		{
-			FieldInfo field = AccessTools.Field(bill.GetType(), field_name)
-				?? AccessTools.Field(typeof(Bill_Production), field_name)
-				?? AccessTools.Field(typeof(Bill), field_name);
-
-			if (field?.FieldType != typeof(string))
-			{
-				continue;
-			}
-
-			field.SetValue(bill, label);
-			return true;
-		}
-
-		return false;
-	}
-
-	private static void SetBetterWorkbenchManagementBillName(Bill_Production bill, string label)
-	{
-		try
-		{
-			Type main_type = GenTypes.GetTypeInAnyAssembly("ImprovedWorkbenches.Main");
-
-			if (main_type == null)
-			{
-				return;
-			}
-
-			object main_instance = AccessTools.Property(main_type, "Instance")?.GetValue(null, null)
-				?? AccessTools.Field(main_type, "Instance")?.GetValue(null);
-
-			if (main_instance == null)
-			{
-				return;
-			}
-
-			MethodInfo storage_getter = AccessTools.Method(main_type, "GetExtendedBillDataStorage");
-			object storage = storage_getter?.Invoke(main_instance, null);
-
-			if (storage == null)
-			{
-				return;
-			}
-
-			MethodInfo get_or_create = AccessTools.Method(storage.GetType(), "GetOrCreateExtendedDataFor", new[] { typeof(Bill_Production) });
-			object extended_data = get_or_create?.Invoke(storage, new object[] { bill });
-			FieldInfo name_field = extended_data == null ? null : AccessTools.Field(extended_data.GetType(), "Name");
-
-			if (name_field?.FieldType == typeof(string))
-			{
-				name_field.SetValue(extended_data, label);
-			}
-		}
-		catch (Exception exception)
-		{
-			Log.WarningOnce($"[SeedsPleaseLiteRedux] Could not set Better Workbench Management bill name: {exception.Message}", 816631452);
-		}
+		bill.RenamableLabel = label;
 	}
 
 	private static void AddBill(Building_WorkTable table, ThingDef produce_def, bool show_message = true)
